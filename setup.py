c13f2a6efeb398728c729ef2dfb02c18151e390c
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2024-03-03 16:14:06 -0500

    Add setup.py file for project configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sleep-stage-classifier',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Machine Learning project to classify sleep stages',
+    author='Ajay Karthick',
+    license='MIT',
+)
