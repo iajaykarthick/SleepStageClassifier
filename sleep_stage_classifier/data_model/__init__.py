f115c370d568e07320fa27faba04a2f36e28652f
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2024-03-04 08:11:26 -0500

    Add PSGHypnogramPair class to data_model package

--- sleep_stage_classifier/data_model/__init__.py
+++ sleep_stage_classifier/data_model/__init__.py
@@ -1,2 +1,15 @@
 from .psg import PSG
 from .hypnogram import Hypnogram
+
+
+class PSGHypnogramPair:
+    def __init__(self, psg_file_path, hypnogram_file_path, session_id=None):
+        self.psg = PSG(psg_file_path)
+        self.hypnogram = Hypnogram(hypnogram_file_path)
+        self.session_id = session_id
+    
+    def get_psg(self):
+        return self.psg
+    
+    def get_hypnogram(self):
+        return self.hypnogram
